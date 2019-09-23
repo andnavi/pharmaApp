9856f59054b501540b1ce9ed3669868e851431ba
Aflak Arshi <aflak.arshi@niyuj.com>
2019-09-23 23:38:35 +0530

    inital client setup

--- config/seed.js
+++ config/seed.js
@@ -1,7 +1,7 @@
-const User = require('../app/models/User')
-const faker = require('faker')
+const User = require('../app/models/User');
+const faker = require('faker');
 const shortid = require('shortid');
-const response = require('../app/libs/responseLib')
+const response = require('../app/libs/responseLib');
 
 let USerModel = ({
     userId:shortid.generate(),
@@ -10,9 +10,9 @@ let USerModel = ({
     password:faker.internet.password(),
     email:faker.internet.email(),
     mobileNumber:faker.random.number()
-})
+});
 
 module.exports = {
     USerModel
-}
+};
 
---
+++ pharma-client/src/app/@core/services/customers.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomersService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCustomers() {
+    return this.http.get(`v1/customers/create`);
+  }
+
+  addCustomers(customerObject: any) {
+    return this.http.post(`v1/customers/create`, customerObject);
+  }
+}
